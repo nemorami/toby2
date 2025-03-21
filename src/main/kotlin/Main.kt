a7ed56b4b1e42a47141fceca3f54975b24c27de0
nemorami <nemorami@daum.net>
2025-03-21 20:11:50 +0900

    1.2.3 DB커넥션 만들기의 독립 NUserDao

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,16 +5,16 @@ import java.sql.DriverManager
 
 data class User(val id: String, val name: String, val password: String)
 
-class UserDao {
+abstract class UserDao {
     fun add(user: User) {
         getConnection().use {c ->
-           c.prepareStatement("INSERT INTO users (id, name, password) VALUES (?, ?, ?)").use {ps ->
-               ps.setString(1, user.id)
-               ps.setString(2, user.name)
-               ps.setString(3, user.password)
+            c.prepareStatement("INSERT INTO users (id, name, password) VALUES (?, ?, ?)").use {ps ->
+                ps.setString(1, user.id)
+                ps.setString(2, user.name)
+                ps.setString(3, user.password)
 
-               ps.executeUpdate()
-           }
+                ps.executeUpdate()
+            }
         }
     }
 
@@ -30,7 +30,15 @@ class UserDao {
         }
     } // end of fun get()
 
-    private fun getConnection(): Connection {
+    abstract fun getConnection(): Connection
+
+//    private fun getConnection(): Connection {
+//        Class.forName("org.postgresql.Driver")
+//        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/nemorami", "nemorami", "j5nfants")
+//    }
+}
+class NUserDao : UserDao() {
+    override fun getConnection(): Connection {
         Class.forName("org.postgresql.Driver")
         return DriverManager.getConnection("jdbc:postgresql://localhost:5432/nemorami", "nemorami", "j5nfants")
     }
@@ -39,7 +47,7 @@ class UserDao {
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val user = User("John", "Peter", "123456")
-    val dao = UserDao()
+    val dao = NUserDao()
     dao.add(user)
 
     println("${dao.get(user.id)} 등록성공")
